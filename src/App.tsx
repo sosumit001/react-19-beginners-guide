a89457e23d47f0090cf507796514808e007d2be5
sumitm001 <sumit.m@reidnax.com>
2024-05-05 00:35:34 +0530

    added useActionState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,48 @@
-import React from "react";
+<<<<<<< HEAD
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useActionState, useEffect, useState } from "react"
+import UserTable from "./components/@ui/table"
+=======
+>>>>>>> 7e48df7 (react-actions)
+
+import React from "react"
+
 const App = () => {
+
+<<<<<<< HEAD
+	const [users, setUsers] = useState<TUser[] | null>(null)
+
+	const [_error , submitAction, isPending] = useActionState(
+		async (_prevState: any, formData: FormData) => {
+			const username = formData.get("name")
+			if (!username) {
+				alert("Username cannot be empty");
+				return;
+			}
+			await fetch('http://localhost:3001/users', {
+				method: "POST",
+				body: JSON.stringify({name: username})
+			})
+		},
+		null
+	)
+	
+	const fetchUserData = async () => {
+		const response = await fetch('http://localhost:3001/users')
+		const data = await response.json()
+		setUsers(data)
+	}
+
+	useEffect(() => {
+		fetchUserData()
+	}, [isPending])
+=======
+>>>>>>> 7e48df7 (react-actions)
+
 	return (
 		<div>
-			Hello, world
+			hello, world
 		</div>
 	);
 }
