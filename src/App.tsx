b8e84133fa4ddf1780634cba4fa85f767b4876d7
sumitm001 <sumit.m@reidnax.com>
2024-05-05 00:35:34 +0530

    added useActionState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,60 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useState, useTransition } from "react"
+import UserTable from "./components/@ui/table"
+
+type TUser = {
+	id: number,
+	name: string
+}
+
 const App = () => {
+
+	const [users, setUsers] = useState<TUser[] | null>(null)
+	const [username, setUsername] = useState<string | null>(null) 
+	const [isPending, startTransition] = useTransition()
+
+	const handleAddUser = () => {
+		startTransition(async  () => {
+			if (!username) {
+				alert("Username cannot be empty");
+				return;
+			}
+			await fetch('http://localhost:3001/users', {
+				method: "POST",
+				body: JSON.stringify({name: username})
+			})
+		})
+	}
+	
+	const fetchUserData = async () => {
+		const response = await fetch('http://localhost:3001/users')
+		const data = await response.json()
+		setUsers(data)
+	}
+
+	useEffect(() => {
+		fetchUserData()
+	}, [isPending])
+
 	return (
 		<div>
-			Hello, world
+			<h1>React 19 : Actions</h1>
+			{/* User's Table */}
+			<div>
+				<input 
+				type="text" 
+				name="name"
+				onChange={(e) => {setUsername(e.target.value)}}
+				/>
+				<button type="submit" disabled={isPending}>Add User</button>
+			{
+				users?
+				<UserTable users={users} />
+				:
+				<p>loading...</p>
+			}
+			</div>
 		</div>
 	);
 }
