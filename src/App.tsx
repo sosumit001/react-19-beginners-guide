7e48df7962d2123676e7fbdc42ef01ea71452ca1
sumitm001 <sumit.m@reidnax.com>
2024-05-05 00:43:23 +0530

    react-actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,12 @@
-import React, { useActionState, useEffect, useState } from "react"
-import UserTable from "./components/@ui/table"
 
-type TUser = {
-	id: number,
-	name: string
-}
+import React from "react"
 
 const App = () => {
 
-	const [users, setUsers] = useState<TUser[] | null>(null)
-
-	const [error, submitAction, isPending] = useActionState(
-		async (prevState: any, formData: FormData) => {
-			const username = formData.get("name")
-			if (!username) {
-				alert("Username cannot be empty");
-				return;
-			  }
-			await fetch('http://localhost:3001/users', {
-				method: "POST",
-				body: JSON.stringify({name: username})
-			})
-		},
-		null
-	)
-	
-	const fetchUserData = async () => {
-		const response = await fetch('http://localhost:3001/users')
-		const data = await response.json()
-		setUsers(data)
-	}
-
-	useEffect(() => {
-		fetchUserData()
-		console.log(isPending)
-	}, [isPending])
 
 	return (
 		<div>
-			<h1>React 19 : Actions</h1>
-			{/* User's Table */}
-			<form action={submitAction}>
-				<input 
-				type="text" 
-				name="name"
-				/>
-				<button type="submit" disabled={isPending}>Add User</button>
-			{
-				users?
-				<UserTable users={users} />
-				:
-				<p>loading...</p>
-			}
-			</form>
+			hello, world
 		</div>
 	);
 }
