8e31eb5e6b26e81cd62c376d1085bf7599fb7ad4
sumitm001 <sumit.m@reidnax.com>
2024-05-05 00:35:34 +0530

    added useActionState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React from "react"
+
 const App = () => {
+
 	return (
 		<div>
-			Hello, world
+			hello, world
 		</div>
 	);
 }
--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const container = document.getElementById("app");
-if(container != null) {
-	const root = createRoot(container)
-	root.render(<App />);
-}
